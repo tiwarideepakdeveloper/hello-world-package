db2f47cd674bdbf1aff7148172945ed748cd51ee
tiwarideepakdeveloper <tiwarideepak.developer@gmail.com>
2024-01-14 23:44:42 +0530

    File Genrate

--- src/HelloWorld.php
+++ src/HelloWorld.php
@@ -6,6 +6,14 @@ class HelloWorld
 {
     public static function sayHello()
     {
-        echo "Hello World";
+        $rootDir = getcwd();
+
+        // Example: Creating a file in the root directory
+        $filePath = $rootDir . '/example-file.txt';
+        $fileContent = 'Hello, this file was created by your Composer package!';
+        
+        file_put_contents($filePath, $fileContent);
+
+        echo "File created at: $filePath\n";
     }
 }
